95afb4085f091b88b34e4c00ecf78923b725febb
Thiago Sudré Cezarino <thiagoty@gmail.com>
2020-08-03 14:56:05 -0300

    [FIX] Corrigindo tweets buscados

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 app.listen(server_port, server_host, function () {
     console.log("Aplicação online.");
-    buscado = ["um like e", "1 like e", "01 like e", "um fav e", "1 fav e", "01 fav e"]
+    buscado = [     ]
+    buscado = [
+        "um like e",
+        "1 like e",
+        "01 like e",
+        "um fav e",
+        "1 fav e",
+        "01 fav e",
+        "um like aqui",
+        "1 like aqui",
+        "01 like aqui",
+        "um fav aqui",
+        "1 fav aqui",
+        "01 fav aqui",
+    ]
     bot.buscaTweet(buscado);
 });
