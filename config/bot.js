51930a1d0999e024af8b54ba1e5469e94705869f
Thiago Sudré Cezarino <thiagoty@gmail.com>
2020-09-21 10:32:14 -0300

    Revert "[ADD] Remoção de asteriscos do tweet"
    This reverts commit 3b62c49ca760bade0345367b3a364b9344119acd.

--- config/bot.js
+++ config/bot.js
@@ -60,9 +60,6 @@ bot.reverificarSubString = function (textoTweet) {
         "deleto minha conta",
         "cometo",
         "m4t0",
-        "****",
-        "*******",
-        "********",
     ];
     
     var retorno = false;
