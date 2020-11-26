575088cc2354d4c58e695da3260b760fb6a880a6
thisudre <thiagoty@gmail.com>
2020-11-26 16:19:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var app = require('./config/server.js');
 const bot = require('./config/bot.js');
+const gerador = require('./models/gerador-de-buscados.js');
 
 //Configura a porta disponível ou a porta 3000
 var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
@@ -8,19 +9,8 @@ var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 app.listen(server_port, server_host, function () {
     console.log("Aplicação online.");
-    buscado = [
-        "um like e",
-        "1 like e",
-        "01 like e",
-        "um fav e",
-        "1 fav e",
-        "01 fav e",
-        "um like aqui",
-        "1 like aqui",
-        "01 like aqui",
-        "um fav aqui",
-        "1 fav aqui",
-        "01 fav aqui"
-    ];
+    buscado = [];
+    buscado = gerador.gerarTextoBuscado(buscado);
+    console.log(buscado);
     bot.buscaTweet(buscado);
 });
