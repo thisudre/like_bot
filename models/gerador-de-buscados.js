e3d055d5e1481efb7389c3ed18876cac6bf9d3ce
thisudre <thiagoty@gmail.com>
2020-11-26 16:16:33 -0300

    Create gerador-de-buscados.js

---
+++ models/gerador-de-buscados.js
@@ -0,0 +1,40 @@
+const gerador = {};
+
+gerador.palavrasChave = {
+	num: [
+		"um",
+		"dois",
+		"dez",
+		"Um",
+		"Dois",
+		"Dez",
+		"1",
+		"2",
+		"10",
+		"01",
+		"02"
+	],
+	finais: [
+		"like e",
+		"like eu",
+		"like aqui",
+		"fav e",
+		"fav eu",
+		"fav aqui"
+	]
+};
+
+gerador.gerarTextoBuscado = (vetorVazio) => {
+	gerador.palavrasChave.num.forEach(
+		num => {
+			gerador.palavrasChave.finais.forEach(
+				final => {
+					vetorVazio.push(num + ' ' + final);
+				}
+			);
+		}
+	);
+	return vetorVazio;
+};
+
+module.exports = gerador;
