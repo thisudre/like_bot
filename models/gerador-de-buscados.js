0591b3a5675446206a7af4ce89e7737c1b084e1b
Thiago Sudré Cezarino <thiagoty@gmail.com>
2022-01-14 22:42:04 -0300

    Adição do final pra Dalla

--- models/gerador-de-buscados.js
+++ models/gerador-de-buscados.js
@@ -1,39 +1,24 @@
 const gerador = {};
 
 gerador.palavrasChave = {
-	num: [
-		'um',
-		'dois',
-		'dez',
-		'Um',
-		'Dois',
-		'Dez',
-		'1',
-		'2',
-		'10',
-		'01',
-		'02'
-	],
+	num: ['um', 'dois', 'dez', 'Um', 'Dois', 'Dez', '1', '2', '10', '01', '02'],
 	finais: [
 		'like e',
 		'like eu',
 		'like aqui',
+		'like nesse tweet',
 		'fav e',
 		'fav eu',
-		'fav aqui'
-	]
+		'fav aqui',
+	],
 };
 
 gerador.gerarTextoBuscado = (vetorVazio) => {
-	gerador.palavrasChave.num.forEach(
-		num => {
-			gerador.palavrasChave.finais.forEach(
-				final => {
-					vetorVazio.push(num + ' ' + final);
-				}
-			);
-		}
-	);
+	gerador.palavrasChave.num.forEach((num) => {
+		gerador.palavrasChave.finais.forEach((final) => {
+			vetorVazio.push(num + ' ' + final);
+		});
+	});
 	return vetorVazio;
 };
 
